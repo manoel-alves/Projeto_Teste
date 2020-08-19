ceb5f280556099410931187a78047255b807757b
Manoel Alves <manoelann@hotmail.com>
2020-08-19 13:09:31 -0300

    Função para dar bom dia/boa tarde/boa noite de acordo com a hora local

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -4,7 +4,21 @@ namespace Projeto_Teste {
     class Program {
         static void Main(string[] args) {
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo,");
+
+            DateTime now = DateTime.Now;
+            int horas = now.Hour;
+
+            if (horas >= 18 && horas < 24) {
+                Console.WriteLine("Boa Noite!");
+            } 
+            else if (horas > 12) {
+                Console.WriteLine("Boa Tarde!");
+            }
+            else {
+                Console.WriteLine("Bom dia!");
+            }
+
 
             Console.ReadLine();
 
