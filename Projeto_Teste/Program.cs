8ef0855ca356756f962d703e54966704ed350030
Manoel Alves <manoelann@hotmail.com>
2020-08-20 20:44:41 -0300

    Formatação padrão do código

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 
-namespace Projeto_Teste {
-    class Program {
-        static void Main(string[] args) {
-            
+namespace Projeto_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
             Console.WriteLine("Olá Mundo,");
 
             DateTime now = DateTime.Now;
             int horas = now.Hour;
 
-            if (horas >= 18 && horas < 24) {
+            if (horas >= 18 && horas < 24)
+            {
                 Console.WriteLine("Boa Noite!");
-            } 
-            else if (horas > 12) {
+            }
+            else if (horas > 12)
+            {
                 Console.WriteLine("Boa Tarde!");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Bom dia!");
             }
 
